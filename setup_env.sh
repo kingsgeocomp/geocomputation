dcfbb9501a3b6590ce72ae3fa360bed5562a3baa
Jon Reades <jon@reades.com>
2017-09-04 12:09:28 +0100

    Trying to debug mac issue with package channel default

--- setup_env.sh
+++ setup_env.sh
@@ -9,7 +9,7 @@ conda update --quiet --yes conda
 # conda env remove -n gsa2017
 
 printf "Creating environment\n"
-conda create -n "$ENVNM" -y -f python=2.7 anaconda
+conda create -c conda-forge --override-channels -name "$ENVNM" -y -f python=2.7 anaconda
 
 printf "Activating environment\n"
 source activate "$ENVNM"
@@ -18,10 +18,10 @@ printf "Insalling required notebook packages\n"
 conda install -y ipython jupyter nb_conda
 
 printf "Installing default packages\n"
-conda install -y matplotlib numpy scipy seaborn=0.8* scikit-learn=0.18* statsmodels=0.8.0 basemap requests 
+conda install -y -c conda-forge matplotlib numpy scipy seaborn=0.8* scikit-learn=0.18* statsmodels=0.8.0 basemap requests 
 
 printf "Installing packages from conda-forge...\n"
-conda install --y -c conda-forge clusterpy=0.9.9 geopandas=0.2* palettable=3.* pysal=1.13* folium mplleaflet rasterio fiona
+conda install --y -c conda-forge clusterpy=0.9.9 geopandas=0.2* palettable=3.* pysal=1.13* folium mplleaflet rasterio 
 
 # From Source
 printf "Installing final packages via pip...\n"
@@ -29,7 +29,7 @@ pip install cenpy
 pip install sompy
 
 printf "Exporting to YAML for cloning...\n"
-conda env export > "$ENVNM.yml
+conda env export > "$ENVNM.yml"
 
 printf "Done.\n"
 source deactivate "$ENVNM"
