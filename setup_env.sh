13f7a26f8102917b2dcd3cb1d149e22ec9c1c310
Jon Reades <jon@reades.com>
2017-09-04 15:36:03 +0100

    Sorting out testing infrastructure. Seems to work now.

--- setup_env.sh
+++ setup_env.sh
@@ -2,26 +2,34 @@
 
 ENVNM="gsa2017"
 
+# Set channel priority
+conda config --prepend channels conda-forge
+
 printf "Updating conda...\n"
 conda update --quiet --yes conda
 
-# If already exists:
-# conda env remove -n gsa2017
+# If already exists (does not default to 'yes'):
+conda env remove -n "$ENVNM"
 
 printf "Creating environment\n"
-conda create -c conda-forge --override-channels -name "$ENVNM" -y -f python=2.7 anaconda
+conda create -n "$ENVNM" -y python=2.7.13 anaconda
+#conda create --channel conda-forge --override-channels -n gsa2017 python=2.7 anaconda
 
 printf "Activating environment\n"
 source activate "$ENVNM"
+
+# Update anaconda
+printf "Updating anaconda package defaults...\n"
+conda update anaconda
 
 printf "Insalling required notebook packages\n"
 conda install -y ipython jupyter nb_conda
 
 printf "Installing default packages\n"
-conda install -y -c conda-forge matplotlib numpy scipy seaborn=0.8* scikit-learn=0.18* statsmodels=0.8.0 basemap requests 
+conda install -y matplotlib numpy scipy seaborn=0.8* scikit-learn=0.18* statsmodels=0.8* basemap requests 
 
 printf "Installing packages from conda-forge...\n"
-conda install --y -c conda-forge clusterpy=0.9.9 geopandas=0.2* palettable=3.* pysal=1.13* folium mplleaflet rasterio 
+conda install --y clusterpy=0.9.9 geopandas=0.2* palettable=3.* pysal=1.13* folium mplleaflet rasterio 
 
 # From Source
 printf "Installing final packages via pip...\n"
